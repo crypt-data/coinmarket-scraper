1b346bdc437984a00f9fd2a6a5bffc65947f5eca
atec <root@atec.pub>
2017-12-03 18:58:52 -0500

    draft out structs

--- main.go
+++ main.go
@@ -9,6 +9,32 @@ import (
 	"github.com/kr/pretty"
 )
 
+type Response struct {
+	Status            string               `json:"Response"`
+	Type              int                  `json:"Type"`
+	Aggregated        bool                 `json:"Aggregated"`
+	Data              []Tick               `json:"Data"`
+	TimeTo            int                  `json:"TimeTo"`
+	TimeFrom          int                  `json:"TimeFrom"`
+	FirstValueInArray bool                 `json:"FirstValueInArray"`
+	ConversionType    ConversionTypeStruct `json:"ConversionType"`
+}
+
+type Tick struct {
+	Time       int     `json:"time"`
+	Close      float64 `json:"close"`
+	High       float64 `json:"high"`
+	Low        float64 `json:"low"`
+	Open       float64 `json:"open"`
+	VolumeFrom float64 `json:"volumefrom"`
+	VolumeTo   float64 `json:"volumeto"`
+}
+
+type ConversionTypeStruct struct {
+	Type             string `json:"type"`
+	ConversionSymbol string `json:"conversionSymbol"`
+}
+
 func main() {
 
 	var url = url.URL{
